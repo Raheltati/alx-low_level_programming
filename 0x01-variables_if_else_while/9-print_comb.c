bbf19613149a8195438bc0e5bd7d2f2a5873efc9
Raheltati <rtw3010@gmail.com>
2023-03-18 01:10:36 -0700

    8 print combo num

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program  prints all possible combinations of single-digit numbers
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		if (n == 9)
+			putchar(n + '0');
+		else
+			putchar(n + '0');
+			putchar(',');
+			putchar(' ');
+	}
+
+	return (0);
+}
