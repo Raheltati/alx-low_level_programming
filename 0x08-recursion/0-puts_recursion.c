53d190b3b9c704a55b3d4f56c52afe19202fe2df
Raheltati <rtw3010@gmail.com>
2023-04-05 06:10:44 -0700

    recursion 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ *_puts_recursion - prints strin followed by new line
+ *
+ *@s: string
+ *Return: no return
+ */
+
+void _puts_recursion(char *s);
+{
+	if (*s == '\0')
+		printf("\n");
+
+	else
+		printf("%c", *s);
+	_puts_recursion(s + 1);
+}
+
