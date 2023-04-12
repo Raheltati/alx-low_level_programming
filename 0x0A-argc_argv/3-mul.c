41c9198f6fb874ba4c3faa807febf7ff052410f8
Raheltati <rtw3010@gmail.com>
2023-04-12 05:12:33 -0700

    product of 2nums

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+		printf("Error\n");
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = (num1 * num2); /**
+				  *result is declared and initialized to the product of num1 and num2
+				  */
+
+	printf("%d\n", result);
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,5 @@
 #ifndef MAIN_H
 
 int _putchar(char c);
-
+int atoi(const char *str);
 #endif
