a272407781ee57b90b0bcfa0fbbc11cdd2af9aca
Raheltati <rtw3010@gmail.com>
2023-04-12 05:24:25 -0700

    prod of num

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,14 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
 
 	if (argc != 3)
+
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = (num1 * num2); /**
-				  *result is declared and initialized to the product of num1 and num2
-				  */
-
-	printf("%d\n", result);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
